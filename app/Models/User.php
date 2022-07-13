44511eaf1b3433c1863d7cb578244695ff3533d0
Alysmar <alysmaralejandra@gmail.com>
2022-07-13 18:36:08 -0400

    recuperar password con token a email

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,4 +59,5 @@ class User extends Authenticatable implements JWTSubject
     public function getJWTCustomClaims() {
         return [];
     }    
+   
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MailController;
+use App\Http\Controllers\PasswordResetRequestController;
+use App\Http\Controllers\ChangePasswordController;
+
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,5 +26,7 @@ Route::group([
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
-    Route::get('/user-profile', [AuthController::class, 'userProfile']);    
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);  
+    Route::post('/reset-password-request', [PasswordResetRequestController::class, 'sendPasswordResetEmail']);
+    Route::post('/change-password', [ChangePasswordController::class, 'passwordResetProcess']);
 });
